4aae4c9896bca18dd8c633eb9c68a3333cb2cbea
ydang <ydang@futurify.vn>
2019-09-10 20:27:26 +0700

    Remove dashboard component

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -2,21 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { AdminComponent } from './admin.component';
-import { Dashboard1Component } from './../admin/layout/dashboard1/dashboard1.component';
 
 const routes: Routes = [{
   path: 'admin',
   component: AdminComponent,
   children: [
-    {
-      path: '',
-      redirectTo: 'dashboard1',
-      pathMatch: 'full'
-    },
-    {
-      path: 'dashboard1',
-      component: Dashboard1Component
-    }
+    // {
+    //   path: '',
+    //   redirectTo: 'dashboard1',
+    //   pathMatch: 'full'
+    // },
+    // {
+    //   path: 'dashboard1',
+    // }
   ]
 }];
 
