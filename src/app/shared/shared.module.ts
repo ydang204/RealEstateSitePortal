9e0886c2a37223fb59c9b18abc8c493f636e15ca
ydang <ydang@futurify.vn>
2019-09-14 14:02:02 +0700

    Remove share component and create api url environment

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SharedComponent } from './shared.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [SharedComponent]
+  declarations: []
 })
 export class SharedModule { }
